eea09ac1746a0f26094779e41aa632c04943557d
Chris Kim <chris.kim@microchip.com>
2025-12-17 10:44:25 +0900

    add uart func on main

--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 #include <string.h>
 #include <stdio.h>
+#include "uart.h"
 
 int sub(int a, int b)
 {
@@ -9,10 +10,13 @@ int sub(int a, int b)
 int main(void)
 {
 	static int a = 5, b = 3, result;
+	static char msg[20] = "uart msg buf\n";
 
 	result = sub(a, b);
 
 	printf("sub result = %d\n", result);
+
+	uart_tx(msg);
 
     return 0;
 }
